9988c83c32bc01fbbb964e95bfbce24104289e82
elitekentoy <jonathankentmorales@gmail.com>
2025-04-07 17:24:45 +0800

    add asset

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import "net/http"
 func main() {
 	serveMux := http.NewServeMux()
 	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	serveMux.Handle("/asset/", http.FileServer(http.Dir("./asset/")))
 	server := http.Server{
 		Handler: serveMux,
 		Addr:    ":8080",
