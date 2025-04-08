af3b131c820c21f0d8bb2225fec98061d92c29d4
elitekentoy <jonathankentmorales@gmail.com>
2025-04-08 20:20:14 +0800

    add password hashing

--- handler_users.go
+++ handler_users.go
@@ -5,12 +5,15 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/elitekentoy/chirpy/internal/auth"
 	"github.com/elitekentoy/chirpy/internal/database"
+	"github.com/elitekentoy/chirpy/models"
 	"github.com/google/uuid"
 )
 
 type UsersRequestBody struct {
-	Email string `json:"email"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
 func (config *apiConfig) handlerUsers(writer http.ResponseWriter, req *http.Request) {
@@ -24,19 +27,26 @@ func (config *apiConfig) handlerUsers(writer http.ResponseWriter, req *http.Requ
 		return
 	}
 
+	if request.Email == "" || request.Password == "" {
+		http.Error(writer, "email or password cannot be empty", http.StatusBadRequest)
+		return
+	}
+
+	hashedPassword, err := auth.HashPassword(request.Password)
+	if err != nil {
+		http.Error(writer, "cannot hash password", http.StatusInternalServerError)
+		return
+	}
+
 	dbUser, err := config.Database.CreateUser(req.Context(), database.CreateUserParams{
-		ID:        uuid.New(),
-		CreatedAt: time.Now(),
-		UpdatedAt: time.Now(),
-		Email:     request.Email,
+		ID:             uuid.New(),
+		CreatedAt:      time.Now(),
+		UpdatedAt:      time.Now(),
+		Email:          request.Email,
+		HashedPassword: hashedPassword,
 	})
 
-	user := User{
-		ID:        dbUser.ID,
-		CreatedAt: dbUser.CreatedAt,
-		UpdatedAt: dbUser.UpdatedAt,
-		Email:     dbUser.Email,
-	}
+	user := models.UserFromDatabase(dbUser)
 
 	if err != nil {
 		http.Error(writer, "error occured in database", http.StatusInternalServerError)
---
+++ models/user.go
@@ -0,0 +1,24 @@
+package models
+
+import (
+	"time"
+
+	"github.com/elitekentoy/chirpy/internal/database"
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
+}
+
+func UserFromDatabase(dbUser database.User) User {
+	return User{
+		ID:        dbUser.ID,
+		CreatedAt: dbUser.CreatedAt,
+		UpdatedAt: dbUser.UpdatedAt,
+		Email:     dbUser.Email,
+	}
+}
